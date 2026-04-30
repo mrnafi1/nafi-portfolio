eb8fa78f355119ac6e26dc5b208a22b529affaa2
mrnafi1 <nafimr718@gmail.com>
2026-04-30 07:31:09 +0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_REPO_NAME ? `/${env.VITE_REPO_NAME}/` : "/",
+    plugins: [react(), tailwindcss()],
+    server: {
+      open: true,
+      host: false, 
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  };
+});
