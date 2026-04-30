dad64b0103d09b3a8488b4ea37b9543fb0c57e0e
mrnafi1 <nafimr718@gmail.com>
2026-04-30 09:21:53 +0600

    removed blog from navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Profile from "../components/profile/Profile";
 import WorkProcess from "../components/workProcess/WorkProcess";
 import Portfolio from "../components/portfolio/Portfolio";
 import WorkTogether from "../components/workTogether/WorkTogether";
-import Blog from "../components/blog/Blog";
 import Profession from "../components/profession/Profession";
 import HappyClients from "../components/happyClients/HappyClients";
 import Testimonial from "../components/testimonial/Testimonial";
@@ -26,9 +25,7 @@ const Home = () => {
       <div className="bg-gray-900">
         <WorkTogether />
       </div>
-      <div className="blog-background">
-        <Blog />
-      </div>
+     
       <div className="bg-soft-white">
         <Profession />
       </div>
